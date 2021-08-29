067c85ea4de14658a7547df7b08010720d0d1697
Robert Garcia <rgs2@icomp.ufam.edu.br>
2021-08-29 17:59:31 -0400

    feat: Create animated scroll

--- src/pages/Scroll.tsx
+++ src/pages/Scroll.tsx
@@ -1,8 +1,114 @@
 import React from 'react';
-import {View} from 'react-native';
+import {View, Image, Text, StyleSheet} from 'react-native';
+import Animated, {
+  interpolate,
+  useAnimatedScrollHandler,
+  useAnimatedStyle,
+  useSharedValue,
+  Extrapolate,
+} from 'react-native-reanimated';
 
 const Scroll: React.FC = () => {
-  return <View />;
+  const scrollY = useSharedValue(0);
+  const scrollHandler = useAnimatedScrollHandler((event: any) => {
+    scrollY.value = event.contentOffset.y;
+  });
+
+  const headerStyle = useAnimatedStyle(() => {
+    return {
+      height: interpolate(
+        scrollY.value,
+        [0, 180],
+        [300, 120],
+        Extrapolate.CLAMP,
+      ),
+    };
+  });
+
+  const avatarStyle = useAnimatedStyle(() => {
+    return {
+      opacity: interpolate(
+        scrollY.value,
+        [100, 150],
+        [1, 0],
+        Extrapolate.CLAMP,
+      ),
+    };
+  });
+
+  return (
+    <View style={{backgroundColor: '#FFF'}}>
+      <Animated.ScrollView
+        onScroll={scrollHandler}
+        scrollEventThrottle={16}
+        contentContainerStyle={{paddingTop: 300}}>
+        <Text style={styles.listItem}>Item da lista</Text>
+        <Text style={styles.listItem}>Item da lista</Text>
+        <Text style={styles.listItem}>Item da lista</Text>
+        <Text style={styles.listItem}>Item da lista</Text>
+        <Text style={styles.listItem}>Item da lista</Text>
+        <Text style={styles.listItem}>Item da lista</Text>
+        <Text style={styles.listItem}>Item da lista</Text>
+        <Text style={styles.listItem}>Item da lista</Text>
+        <Text style={styles.listItem}>Item da lista</Text>
+        <Text style={styles.listItem}>Item da lista</Text>
+        <Text style={styles.listItem}>Item da lista</Text>
+        <Text style={styles.listItem}>Item da lista</Text>
+        <Text style={styles.listItem}>Item da lista</Text>
+        <Text style={styles.listItem}>Item da lista</Text>
+        <Text style={styles.listItem}>Item da lista</Text>
+        <Text style={styles.listItem}>Item da lista</Text>
+        <Text style={styles.listItem}>Item da lista</Text>
+        <Text style={styles.listItem}>Item da lista</Text>
+        <Text style={styles.listItem}>Item da lista</Text>
+        <Text style={styles.listItem}>Item da lista</Text>
+        <Text style={styles.listItem}>Item da lista</Text>
+      </Animated.ScrollView>
+
+      <Animated.View style={[styles.header, headerStyle]}>
+        <Animated.Image
+          style={[styles.avatar, avatarStyle]}
+          source={{uri: 'https://github.com/Rob30Garcia.png'}}
+        />
+        <Text style={styles.name}>Robert Garcia</Text>
+      </Animated.View>
+    </View>
+  );
 };
+
+const styles = StyleSheet.create({
+  header: {
+    height: 300,
+    backgroundColor: '#6C63FF',
+    paddingVertical: 30,
+    justifyContent: 'flex-end',
+    alignItems: 'center',
+
+    position: 'absolute',
+    overflow: 'hidden',
+    left: 0,
+    right: 0,
+    top: 0,
+  },
+
+  avatar: {
+    height: 140,
+    width: 140,
+    borderRadius: 70,
+    backgroundColor: 'rgba(0,0,0,0.2)',
+  },
+
+  name: {
+    fontSize: 20,
+    fontWeight: 'bold',
+    marginTop: 16,
+    color: '#FFF',
+  },
+
+  listItem: {
+    padding: 20,
+    fontSize: 18,
+  },
+});
 
 export default Scroll;
