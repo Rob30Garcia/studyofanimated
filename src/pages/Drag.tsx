8743d2bc5619db2b0759c4191ffa5b44d05165e3
Robert Garcia <rgs2@icomp.ufam.edu.br>
2021-08-29 18:33:28 -0400

    chore: Animated square

--- src/pages/Drag.tsx
+++ src/pages/Drag.tsx
@@ -1,15 +1,44 @@
 import React from 'react';
 import {View} from 'react-native';
-import Animated from 'react-native-reanimated';
-
-// import { Container } from './styles';
+import {PanGestureHandler} from 'react-native-gesture-handler';
+import Animated, {
+  useAnimatedGestureHandler,
+  useSharedValue,
+  useAnimatedStyle,
+} from 'react-native-reanimated';
 
 const Drag: React.FC = () => {
+  const posX = useSharedValue(0);
+  const posY = useSharedValue(0);
+
+  const onGestureEvent = useAnimatedGestureHandler({
+    onStart: (event: any, ctx: any) => {
+      ctx.posX = posX.value;
+      ctx.posY = posY.value;
+    },
+    onActive: (event: any, ctx: any) => {
+      posX.value = ctx.posX + event.translationX;
+      posY.value = ctx.posY + event.translationY;
+    },
+    onEnd: () => {},
+  });
+
+  const positionStyle = useAnimatedStyle(() => {
+    return {
+      transform: [{translateX: posX.value}, {translateY: posY.value}],
+    };
+  });
+
   return (
     <View style={{flex: 1}}>
-      <Animated.View
-        style={{width: 150, height: 150, backgroundColor: 'red'}}
-      />
+      <PanGestureHandler onGestureEvent={onGestureEvent}>
+        <Animated.View
+          style={[
+            {width: 150, height: 150, backgroundColor: 'red'},
+            positionStyle,
+          ]}
+        />
+      </PanGestureHandler>
     </View>
   );
 };
